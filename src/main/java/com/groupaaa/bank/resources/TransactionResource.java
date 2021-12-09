608107fdb0a79eb80d2ac734275120ae36a60c0a
rossb11 <56341485+rossb11@users.noreply.github.com>
2021-12-09 23:45:30 +0000

    Added @Get request for transaction ID

--- src/main/java/com/groupaaa/bank/resources/TransactionResource.java
+++ src/main/java/com/groupaaa/bank/resources/TransactionResource.java
@@ -5,12 +5,16 @@
  */
 package com.groupaaa.bank.resources;
 
+import com.google.gson.Gson;
 import com.groupaaa.bank.models.Transaction;
 import com.groupaaa.bank.services.TransactionService;
 import java.util.List;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 
 public class TransactionResource {
@@ -21,6 +25,25 @@ public class TransactionResource {
     @Produces(MediaType.APPLICATION_JSON)    
     public List<Transaction> getAllTransactions() {        
        return transactionService.getAllTransactions();
+    }
+    
+//    @GET    
+//    @Produces(MediaType.APPLICATION_JSON)    
+//    public Transaction getTransactionsByID() {        
+//       return transactionService.getTransactionsByID(transactionID);
+//    }
+    
+    @GET
+    @Path("/{transactionID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTransactionJson(@PathParam("transactionId") int transactionId) {
+        Transaction transaction = transactionService.getTransaction(transactionId);
+        
+        if(transaction == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        
+        return Response.ok(new Gson().toJson(transactionId)).build();
     }
     
     
