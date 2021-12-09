7260ab25ceb3efb39daeae8dee3d62e5618e8ba6
rossb11 <56341485+rossb11@users.noreply.github.com>
2021-12-09 23:50:39 +0000

    Added @Get request for balance from account number

--- src/main/java/com/groupaaa/bank/resources/AccountResource.java
+++ src/main/java/com/groupaaa/bank/resources/AccountResource.java
@@ -5,6 +5,8 @@ import com.groupaaa.bank.services.AccountService;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -22,4 +24,12 @@ public class AccountResource {
     public Response getAllAccountsJson() {
         return Response.ok(new Gson().toJson(accountService.getAccountsByCustomerId(customerId))).build();
     }
+    //Get account balance from account number 
+    @GET
+    @Path("/{accountID}/balance")
+    public Response getAccountBalance(@PathParam("accountNo") int accountId){
+        return (Response) accountService.getAccountBalance(accountId);
+    }
+    
+    
 }
